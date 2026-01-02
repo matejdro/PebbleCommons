6c91dbf797e79c17e4c0ac1366a886ad7a03511a
Matej Drobnič <matej@matejdro.com>
2026-01-02 07:45:39 +0100

    refactor(bluetooth-common): cleanup dependencies

--- mobile/bluetooth-common/build.gradle.kts
+++ mobile/bluetooth-common/build.gradle.kts
@@ -7,15 +7,10 @@ dependencies {
    api(libs.dispatch)
    api(libs.pebblekit.api)
 
-   implementation(projects.common)
    implementation(libs.kotlinova.core)
    implementation(libs.kotlin.coroutines)
    implementation(libs.logcat)
    implementation(libs.okio)
 
-   testImplementation(projects.bluetooth.test)
-   testImplementation(projects.bucketsync.test)
-   testImplementation(projects.actionlist.test)
-   testImplementation(projects.tasker.test)
    testImplementation(libs.kotlinova.core.test)
 }
