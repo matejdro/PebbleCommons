e2d8989ce4e559855addaf46836bff0160c49f89
Matej Drobnič <matej@matejdro.com>
2026-01-21 13:03:40 +0100

    feat(watch): add uint32 extraction

--- watch/bytes.c
+++ watch/bytes.c
@@ -4,3 +4,8 @@ inline uint16_t read_uint16_from_byte_array(const uint8_t* bytes, const size_t o
 {
     return (bytes[offset] << 8) | (bytes[offset + 1]);
 }
+
+inline uint32_t read_uint32_from_byte_array(const uint8_t* bytes, const size_t offset)
+{
+    return (bytes[offset] << 24) | (bytes[offset + 1] << 16) | (bytes[offset + 2] << 8) | (bytes[offset + 3]);
+}
--- watch/bytes.h
+++ watch/bytes.h
@@ -2,3 +2,4 @@
 #include <pebble.h>
 
 uint16_t read_uint16_from_byte_array(const uint8_t* bytes, size_t offset);
+uint32_t read_uint32_from_byte_array(const uint8_t* bytes, size_t offset);
